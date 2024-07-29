93f268572b44b663d79e70e1b7b910521aa57e71
GENRIT <OxyCloudMod@yandex.ru>
2024-07-29 23:03:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 
 API_TOKEN = '7240803057:AAG7xMLwBKljEiRpjN7EYEVfANX9BGthUCI'
-OWNER_CHAT_ID = 'Clayni0'  # Замените это на ID чата владельца
+OWNER_CHAT_ID = 1420106372  # Замените это на ваш реальный ID чата
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -14,20 +14,20 @@ def send_welcome(message):
 # Handle registration data
 @bot.message_handler(content_types=['document'])
 def handle_docs(message):
-    if message.chat.username == OWNER_CHAT_ID:
+    if message.chat.id == OWNER_CHAT_ID:
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        
+
         with open("temp_skin.png", 'wb') as new_file:
             new_file.write(downloaded_file)
-        
+
         bot.reply_to(message, "Скин получен и сохранен.")
     else:
         bot.reply_to(message, "Вы не являетесь владельцем этого бота.")
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    if message.chat.username == OWNER_CHAT_ID:
+    if message.chat.id == OWNER_CHAT_ID:
         bot.reply_to(message, "Получены регистрационные данные: " + message.text)
     else:
         bot.reply_to(message, "Вы не являетесь владельцем этого бота.")
