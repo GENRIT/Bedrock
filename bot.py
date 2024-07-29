e5392600af0aeccb18654c8170a3befd528a962b
GENRIT <OxyCloudMod@yandex.ru>
2024-07-30 04:12:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telebot import types
 import os
 
 API_TOKEN = '7240803057:AAG7xMLwBKljEiRpjN7EYEVfANX9BGthUCI'
-OWNER_CHAT_ID = 1420106372  # Замените это на ваш реальный ID чата
+OWNER_CHAT_ID = 1420106372
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -15,20 +15,30 @@ def send_welcome(message):
 # Handle registration data
 @bot.message_handler(content_types=['document'])
 def handle_docs(message):
-    if message.caption and message.chat.id == OWNER_CHAT_ID:
-        caption_parts = message.caption.split('\n')
-        nickname = caption_parts[0].split(': ')[1]
-        email = caption_parts[1].split(': ')[1]
-
+    if message.chat.id == OWNER_CHAT_ID:
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        
-        skin_filename = f"{nickname}_skin.png"
-        with open(skin_filename, 'wb') as new_file:
-            new_file.write(downloaded_file)
-        
-        confirmation_message = f"Получены данные регистрации:\nНик: {nickname}\nПочта: {email}\nСкин сохранен как {skin_filename}"
-        bot.reply_to(message, confirmation_message)
+        file_extension = os.path.splitext(message.document.file_name)[1]
+
+        if file_extension == '.mcpack':
+            mcpack_filename = f"{os.path.splitext(message.document.file_name)[0]}.mcpack"
+            with open(mcpack_filename, 'wb') as new_file:
+                new_file.write(downloaded_file)
+            bot.reply_to(message, f"Файл .mcpack сохранен как {mcpack_filename}")
+        elif file_extension == '.png' and message.caption:
+            caption_parts = message.caption.split('\n')
+            if len(caption_parts) == 2 and ': ' in caption_parts[0] and ': ' in caption_parts[1]:
+                nickname = caption_parts[0].split(': ')[1]
+                email = caption_parts[1].split(': ')[1]
+                skin_filename = f"{nickname}_skin.png"
+                with open(skin_filename, 'wb') as new_file:
+                    new_file.write(downloaded_file)
+                confirmation_message = f"Получены данные регистрации:\nНик: {nickname}\nПочта: {email}\nСкин сохранен как {skin_filename}"
+                bot.reply_to(message, confirmation_message)
+            else:
+                bot.reply_to(message, "Подпись должна содержать ник и почту в формате:\nНик: <ваш ник>\nПочта: <ваша почта>")
+        else:
+            bot.reply_to(message, "Файл не является .mcpack или .png с правильной подписью.")
     else:
         bot.reply_to(message, "Вы не являетесь владельцем этого бота или данные регистрации не были отправлены корректно.")
 
